048b084873694ca40510e59be68bb37716135438
Nikolas Papaioannou <nikolasp@ifi.uio.no>
2018-08-16 20:24:06 +0200

    Update for travis

--- corvix/build.sh
+++ corvix/build.sh
@@ -7,6 +7,6 @@ git clone https://www.kismetwireless.net/git/kismet.git /opt/kismet-2018.git
 cd /opt/kismet-2018.git
 
 ./configure
-make -j 2
-make -j 2 suidinstall
-make -j 2 forceconfigs
+make
+make  suidinstall
+make  forceconfigs
